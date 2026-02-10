bfb32378954dbfe64a257a64c27677280c760f38
Tarun Patidar <168515843+Tarun-428@users.noreply.github.com>
2026-02-10 16:07:44 +0530

    Update page.tsx

--- sweet-shop-frontend/app/page.tsx
+++ sweet-shop-frontend/app/page.tsx
@@ -45,7 +45,7 @@ export default function SweetShopApp() {
     sweetQuantity: "",
   })
 
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://43.205.217.78:8000"
 
   useEffect(() => {
     const token = localStorage.getItem("access_token")
